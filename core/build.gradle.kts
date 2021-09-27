c12218c2b8baf387e163efda92a83a4b7ca067c3
Stefan Thaler <stefan@thaler-it.net>
2021-09-27 13:35:32 +0200

    Remove unused comments

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,32 +7,7 @@ plugins {
 
 kotlin {
     jvm()
-//    js {
-//        browser {
-//            testTask {
-//                useKarma {
-//                    useChromeHeadless()
-//                    webpackConfig.cssSupport.enabled = true
-//                }
-//            }
-//        }
-//    }
-//    val hostOs = System.getProperty("os.name")
-//    val isMingwX64 = hostOs.startsWith("Windows")
-//    val nativeTarget = when {
-//        hostOs == "Mac OS X" -> macosX64("native")
-//        hostOs == "Linux" -> linuxX64("native")
-//        isMingwX64 -> mingwX64("native")
-//        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-//    }
-
-//    nativeTarget.apply {
-//        binaries {
-//            executable {
-//                entryPoint = "main"
-//            }
-//        }
-//    }
+    
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -68,13 +43,5 @@ kotlin {
                 implementation(kotlin("test-junit5"))
             }
         }
-//        val jsMain by getting
-//        val jsTest by getting {
-//            dependencies {
-//                implementation(kotlin("test-js"))
-//            }
-//        }
-//        val nativeMain by getting
-//        val nativeTest by getting
     }
 }
